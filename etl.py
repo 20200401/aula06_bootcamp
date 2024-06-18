d286b4fcf4382931e2c5a60680214f42deac6c52
Jesse <jesseabe.rodrigues@gmail.com>
2024-06-17 21:00:14 -0300

    adicionando solucao do desafio

--- etl.py
+++ etl.py
@@ -17,4 +17,26 @@ def ler_csv(nome_do_arquivo_csv: str) -> list[dict]:
 vendas_itens: list[dict]
 
 vendas_itens = ler_csv(path_arquivo)
-print(vendas_itens)
+
+def filtrar_produtos_nao_entregues(lista: list[dict]) -> list[dict]:
+    #Funcao que filtra produtos onde entrega = True
+    lista_com_produtos_filtrados = []
+    for produto in lista:
+        if produto.get("entregue") == "True":
+            lista_com_produtos_filtrados.append(produto)
+    return lista_com_produtos_filtrados
+
+
+def somar_valores_dos_produtos(lista_com_produtos_filtrados: list[dict]) -> int:
+    #Soma todos os produtos que estao na lista
+    valor_total = 0
+    for produto in lista_com_produtos_filtrados:
+        valor_total += int(produto.get("price"))
+    return valor_total
+
+
+
+lista_de_produtos = ler_csv(path_arquivo)
+produtos_entregues = filtrar_produtos_nao_entregues(lista_de_produtos)
+valos_dos_produtos_entregues = somar_valores_dos_produtos(produtos_entregues)
+print(valos_dos_produtos_entregues)
